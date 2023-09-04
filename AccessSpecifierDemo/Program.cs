528c19bcaa592e5ab215721f4694bded14c388cc
Fozle Rabbi <Fozle Rabbi@DESKTOP-DVOA1GO>
2023-09-04 12:15:21 +0600

    Add project files.

---
+++ AccessSpecifierDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using AssemblyOne;
+
+namespace AccessSpecifierDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class AnotherAssemblyClass1 : AssemblyOneClass1
+    {
+        public void Display4()
+        {
+            //You can't access the Member from the Derived Class 
+            //From other Assemblies
+            Console.WriteLine(Id);  //Compile Time Error
+        }
+    }
+
+
+    public class AnotherAssemblyClass2
+    {
+        public void Display3()
+        {
+            //You can't access the Private Member from the Non-Derived Classes 
+            //From other Assemblies
+
+            AnotherAssemblyClass1 obj = new AnotherAssemblyClass1();
+            Console.WriteLine(obj.Id); //Compile Time Error
+        }
+    }
+}
---
+++ AssemblyOne/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AssemblyOne
+{
+    public class AssemblyOneClass1
+    {
+        private int Id;
+        public void Display()
+        {
+            //Private Member Accessible with the Containing Type only
+            //Where they are created, they are available only withing that type
+            Console.WriteLine(Id);
+        }
+    }
+
+    public class AssemblyOneClass2 : AssemblyOneClass1
+    {
+        public void Display2()
+        {
+            //You cannot access the Private Member from the Derived Class
+            //Withing the same Assembly
+            Console.WriteLine(Id); //Compile Time Error
+        }
+    }
+
+    public class AssemblyOneClass3
+    {
+        public void Display3()
+        {
+            //You can't access the Private Member from the Non-Derived Classes
+            //Within the same Assembly
+
+            AssemblyOneClass1 obj = new AssemblyOneClass1();
+            Console.WriteLine(obj.Id);
+        }
+    }
+}
